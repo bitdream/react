cb9ac0651eeef0a4aac1ce849041ee685e99711c
kara <xiliangxu@gmail.com>
2020-04-19 20:03:01 +0800

    Updates

---
+++ precache-manifest.7991364f96bff52d02fe7f20a89c86ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9a882b54a4b68ffa56fb9d5edc502a",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "256803f943e0613a14ea",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ba1d1ced6b74f17679c5",
+    "url": "/react/static/js/2.4b97d4eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.4b97d4eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256803f943e0613a14ea",
+    "url": "/react/static/js/main.30d60574.chunk.js"
+  },
+  {
+    "revision": "9a7d65b3b7b106087613",
+    "url": "/react/static/js/runtime-main.5277ddfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
